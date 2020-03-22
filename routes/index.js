b49d02efd4de1712f99407f672eb8a1747d81f23
GarikGelios <garikgelios@gmail.com>
2020-03-22 19:55:31 +0300

    add ip65 info

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PolSemena.by — Световая новогодняя иллюминация для улицы и декоративная светотехника' });
+  res.render('index', {company: 'ip65', title: 'Световая новогодняя иллюминация для улицы и декоративная светотехника'});
 });
 
 module.exports = router;
